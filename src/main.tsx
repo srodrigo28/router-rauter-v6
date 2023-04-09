08555d47ba052713eadfcbeb75be65db527126d2
SEBASTIAO RODRIGO NASCIMENTO SOUSA <rodrigoexer1@gmail.com>
2023-04-09 20:55:10 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Home } from './pages/Home'
+import { Register } from './pages/Register'
+import { Contato } from './pages/Contato'
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/cadastro", element: <Register /> },
+  { path: "/contato", element: <Contato /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
 )
